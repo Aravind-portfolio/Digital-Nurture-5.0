dff94b8025f88c0bbd32300ca6a257a13254f312
maara@2524 <aravindmurugesan030@gmail.com>
2026-07-25 12:47:35 +0530

    JUnit Ex 1

---
+++ JUnit/Exercise1_SettingUpJUnit/src/test/java/com/cognizant/junit/AppTest.java
@@ -0,0 +1,18 @@
+package com.cognizant.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+
+        App app = new App();
+
+        int result = app.add(2, 3);
+
+        assertEquals(5, result);
+    }
+}
